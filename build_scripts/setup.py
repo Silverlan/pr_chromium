1f83bfa53aa907044c789a6a8dbb1177fbe5a0b4
Silverlan <silverlann@gmail.com>
2022-12-03 11:02:49 +0100

    Replace shell build scripts with python scripts

---
+++ build_scripts/setup.py
@@ -0,0 +1,34 @@
+import os
+from sys import platform
+import tarfile
+from pathlib import Path
+
+os.chdir(deps_dir)
+
+########## CEF ##########
+if platform == "linux":
+	suffix = "linux64"
+else:
+	suffix = "windows64"
+
+cefRoot = deps_dir +"/cef_binary_107.1.9+g1f0a21a+chromium-107.0.5304.110_" +suffix
+if not Path(cefRoot).is_dir():
+	print_msg("CEF not found. Downloading...")
+	tarName = "cef_binary_107.1.9+g1f0a21a+chromium-107.0.5304.110_" +suffix +".tar"
+	http_extract("https://cef-builds.spotifycdn.com/cef_binary_107.1.9%2Bg1f0a21a%2Bchromium-107.0.5304.110_" +suffix +".tar.bz2",tarName,"tar.bz2")
+
+print_msg("Building CEF...")
+os.chdir(cefRoot)
+mkdir("build",cd=True)
+cmake_configure("..",generator)
+cmake_build("Release",["libcef_dll_wrapper"])
+
+cmake_args.append("-DDEPENDENCY_CHROMIUM_INCLUDE=" +cefRoot +"")
+cmake_args.append("-DDEPENDENCY_CEF_LOCATION=" +cefRoot +"")
+
+if platform == "linux":
+	cmake_args.append("-DDEPENDENCY_CHROMIUM_LIBRARY=" +cefRoot +"/Release/libcef.so")
+	cmake_args.append("-DDEPENDENCY_LIBCEF_DLL_WRAPPER_LIBRARY=" +cefRoot +"/build/libcef_dll_wrapper/libcef_dll_wrapper.a")
+else:
+	cmake_args.append("-DDEPENDENCY_CHROMIUM_LIBRARY=" +cefRoot +"/Release/libcef.lib")
+	cmake_args.append("-DDEPENDENCY_LIBCEF_DLL_WRAPPER_LIBRARY=" +cefRoot +"/build/libcef_dll_wrapper/Release/libcef_dll_wrapper.lib")
