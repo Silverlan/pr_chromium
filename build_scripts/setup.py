04eb70014c1cc687ba70d4c3db8b5d53a2f9873a
SlawekNowy <38943477+SlawekNowy@users.noreply.github.com>
2023-03-16 17:20:31 +0100

    fix: fix linux build errors (#2)
    * Fixes for Ninja Multi-Config
    
    Temporary change to debug wrapper for pr_chromium debugging. This will be reverted soon-ish.
    
    * Fixing CEF compiling error on some Linux systems.
    
    * Change to Release
    
    ---------
    
    Co-authored-by: slawekwaga <slawekwaga@gmail.com>

--- build_scripts/setup.py
+++ build_scripts/setup.py
@@ -2,6 +2,8 @@ import os
 from sys import platform
 import tarfile
 from pathlib import Path
+from urllib.parse import quote
+
 
 os.chdir(deps_dir)
 
@@ -11,16 +13,26 @@ if platform == "linux":
 else:
 	suffix = "windows64"
 
-cefRoot = deps_dir +"/cef_binary_107.1.9+g1f0a21a+chromium-107.0.5304.110_" +suffix
+cefVer = "cef_binary_110.0.28+g16a2153+chromium-110.0.5481.104"
+
+cefRoot = deps_dir +"/"+cefVer+"_" +suffix
 if not Path(cefRoot).is_dir():
 	print_msg("CEF not found. Downloading...")
-	tarName = "cef_binary_107.1.9+g1f0a21a+chromium-107.0.5304.110_" +suffix +".tar"
-	http_extract("https://cef-builds.spotifycdn.com/cef_binary_107.1.9%2Bg1f0a21a%2Bchromium-107.0.5304.110_" +suffix +".tar.bz2",tarName,"tar.bz2")
+	tarName = "cef_binary_"+cefVer+"_" +suffix +".tar"
+	http_extract("https://cef-builds.spotifycdn.com/"+quote(cefVer)+"_" +suffix +".tar.bz2",tarName,"tar.bz2")
 
+
+if platform == "linux":
+	import patch
+
+	patchData = patch.fromfile(moduleDir+"/build_scripts/patches/0001-fix-trivially-copyable-cef.patch")
+	if patchData:
+		print_msg("Patching CEF...")
+		patchData.apply(1,cefRoot)
 print_msg("Building CEF...")
 os.chdir(cefRoot)
 mkdir("build",cd=True)
-cmake_configure("..",generator)
+cmake_configure("..",generator,["-DCMAKE_BUILD_TYPE=Release"])
 cmake_build("Release",["libcef_dll_wrapper"])
 
 cmake_args.append("-DDEPENDENCY_CHROMIUM_INCLUDE=" +cefRoot +"")
@@ -28,7 +40,11 @@ cmake_args.append("-DDEPENDENCY_CEF_LOCATION=" +cefRoot +"")
 
 if platform == "linux":
 	cmake_args.append("-DDEPENDENCY_CHROMIUM_LIBRARY=" +cefRoot +"/Release/libcef.so")
-	cmake_args.append("-DDEPENDENCY_LIBCEF_DLL_WRAPPER_LIBRARY=" +cefRoot +"/build/libcef_dll_wrapper/libcef_dll_wrapper.a")
+	if(generator=="Ninja Multi-Config"):
+		cmake_args.append("-DDEPENDENCY_LIBCEF_DLL_WRAPPER_LIBRARY=" +cefRoot +"/build/libcef_dll_wrapper/Release/libcef_dll_wrapper.a")
+	else:
+		cmake_args.append("-DDEPENDENCY_LIBCEF_DLL_WRAPPER_LIBRARY=" +cefRoot +"/build/libcef_dll_wrapper/libcef_dll_wrapper.a")
+
 else:
 	cmake_args.append("-DDEPENDENCY_CHROMIUM_LIBRARY=" +cefRoot +"/Release/libcef.lib")
 	cmake_args.append("-DDEPENDENCY_LIBCEF_DLL_WRAPPER_LIBRARY=" +cefRoot +"/build/libcef_dll_wrapper/Release/libcef_dll_wrapper.lib")
