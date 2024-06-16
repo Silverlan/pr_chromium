b8cc18419a5bb1ddf62e283224df2cb6b1d78e29
Silverlan <silverlann@gmail.com>
2024-06-16 18:37:21 +0200

    build(cmake): update installation config

---
+++ Install.cmake
@@ -0,0 +1,6 @@
+pr_install_create_directory(modules/chromium)
+pr_install_targets(pr_chromium pr_chromium_wrapper pr_chromium_subprocess INSTALL_DIR "modules/chromium/")
+
+pr_install_directory("${DEPENDENCY_CEF_LOCATION}/Resources/" INSTALL_DIR "modules/chromium")
+# Copy everything except .lib files (see https://stackoverflow.com/a/43499846/1879228 )
+pr_install_directory("${DEPENDENCY_CEF_LOCATION}/Release/" INSTALL_DIR "modules/chromium" PATTERN "*.lib" EXCLUDE)
