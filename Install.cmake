b8c967ce95965685bdb9170931c84cb5facbfc7e
Silverlan <silverlan@pragma-engine.com>
2026-04-09 09:41:39 +0200

    build(linux): fix chromium sub-process not being set as executable

--- Install.cmake
+++ Install.cmake
@@ -5,6 +5,6 @@ pr_install_targets(pr_chromium INSTALL_DIR "modules/chromium/")
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/external_libs/pr_chromium_wrapper/cmake/modules")
 pr_install_binaries(cef INSTALL_DIR "modules/chromium/")
 pr_install_files("${chromium_wrapper_RUNTIME}" INSTALL_DIR "modules/chromium/")
-pr_install_files("${chromium_wrapper_EXECUTABLE}" INSTALL_DIR "modules/chromium/")
+pr_install_executable("${chromium_wrapper_EXECUTABLE}" INSTALL_DIR "modules/chromium/")
 
 pr_install_directory("${cef_RESOURCE_DIR}/" INSTALL_DIR "modules/chromium/")
